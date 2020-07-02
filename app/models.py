6091502f32b19e2aa5b94a9bb94f8cd733a3da45
kanika <kanika.s@infoedge.com>
2020-07-02 22:04:21 +0530

    login Manager.

--- app/models.py
+++ app/models.py
@@ -1,15 +1,16 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+from flask_login import UserMixin
 
-
-class User(db.Model):
+class User(UserMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index=True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
     password_hash = db.Column(db.String(128))
     contact = db.Column(db.String(10))
+    blogs = db.relationship('Blog', backref="author", lazy="dynamic")
 
     def checkPwd(self, pwd):
         return check_password_hash(self.password_hash, pwd)
@@ -22,9 +23,6 @@ def set_pwd_hash( pwd):
     return  generate_password_hash(pwd)
 
 
-
-
-
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -34,3 +32,4 @@ class Blog(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,40 @@
-from flask import render_template, flash, redirect, url_for
-from app import application, db
+from flask import render_template, flash, redirect, url_for, request
+from app import application, db, login
 from app.Form import LoginForm, RegistrationForm
 from app.models import User, set_pwd_hash
+from flask_login import login_required, current_user, login_user, logout_user
+from werkzeug.urls import url_parse
 
 @application.route('/')
 @application.route('/index')
-def index(user, blog):
-    return render_template('index',user = user, data = blog)
+@login_required
+def index():
+    return render_template('index.html')
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+@application.route('/welcome')
+@login_required
+def welcome():
+    return render_template('Welcome.html')
 
 @application.route('/login', methods= ['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-
         user = User.query.filter_by(username=form.username.data).first()
-
         if user and user.checkPwd(form.password.data):
             flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
             # blogs = Blog.query.filter_by(user_id= user.id)
-            return render_template('index.html',user = user, data = "")
-
+            login_user(user, remember=form.remember_me.data)
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('index')
+            return redirect(next_page)
     return render_template('login.html', form=form)
 
 @application.route('/register', methods = ['GET', "POST"])
@@ -44,3 +59,8 @@ def register():
             return redirect(url_for('login'))
     else:
         return render_template('registration.html', form=form )
+
+@application.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
